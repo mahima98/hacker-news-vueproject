1e56166f1001664c308ee5cedd5dad56a6209243
Mahima Ramgolam <mahima@ramgolam.com>
2020-12-02 21:58:58 +0400

    added item.vue

---
+++ src/views/item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="item">
+    <div class="container mx-auto">
+      <Header />
+
+      <div class="news">
+        <div
+          class="bg-gray-200  border-r-4 border-l-4 border-b-4 border-blue-400"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
