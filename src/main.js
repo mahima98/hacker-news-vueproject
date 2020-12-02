4b1ab4bf06c37324f2afd2ee887994a9f7be57df
Mahima Ramgolam <mahima@ramgolam.com>
2020-12-02 20:15:04 +0400

    main.js updated

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import VueResource from 'vue-resource';
+Vue.use(VueResource)
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  VueResource,
   render: function(h) {
     return h(App);
   }
