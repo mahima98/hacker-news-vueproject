670428973c499e8c5309a94278d6b10a7b57a012
Mahima Ramgolam <mahima@ramgolam.com>
2020-12-03 15:52:24 +0400

    cmtpage.vue updated

---
+++ src/views/Cmtpage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="CmtPage">
+    <div class="container mx-auto">
+      <Header />
+
+      <div class="news">
+        <div
+          class="bg-gray-200  border-r-4 border-l-4 border-b-4 border-blue-400"
+        >
+          <Cmtbox />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Header from "../components/Header.vue";
+import Cmtbox from "../components/Cmtbox.vue";
+
+export default {
+  components: {
+    Header,
+    Cmtbox,
+  },
+};
+</script>
