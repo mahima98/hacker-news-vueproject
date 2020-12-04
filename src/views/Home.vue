9605c1bcc9b5dce3ab862dae97f177e759e5852c
Mahima Ramgolam <mahima@ramgolam.com>
2020-12-04 15:07:37 +0400

    Home.vue updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,14 @@
 <template>
   <div class="home">
-    <div class="container mx-auto">
+    <div class="container mx-auto pb-10">
       <Header />
       <div class="topstories">
         <div class="bg-gray-200 py-4">
-          <TopStories
-            v-for="(item, index) in items"
+          <TopStory
+            v-for="(item, index) in items.slice(0, 8)"
             :key="index"
             :values="item"
           />
-        </div>
-      </div>
-      {{ item }}
-      <div class="news">
-        <div
-          class="bg-gray-200  border-r-4 border-l-4 border-b-4 border-blue-400"
-        >
-          <div class="item-box">
-            <div class="flex">
-              <div class="Number flex justify-center p-4 space-x-2">
-                <div class="text-2xl">1.</div>
-                <div>
-                  <button>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="31"
-                      viewBox="0 0 24 24"
-                      fill="white"
-                      stroke="currentColor"
-                      stroke-width="0"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="feather feather-triangle"
-                    >
-                      <path
-                        d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-              </div>
-              <div class="info p-4">
-                <router-link
-                  to=""
-                  class="title text-2xl hover:underline hover:font-extrabold"
-                >
-                  {{ items.title }}
-                </router-link>
-                <div class="flex py-2">
-                  <button
-                    class="hover:underline text-gray-500 border-r-2 border-gray-400 pr-4"
-                  >
-                    {{ items.score }} points by {{ items.by }}
-                  </button>
-                  <button class="hover:underline text-gray-500 px-4">
-                    View {{ items.descendants }} comments
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -69,13 +17,13 @@
 
 <script>
 import Header from "../components/Header.vue";
-import TopStories from "../components/TopStories.vue";
+import TopStory from "../components/TopStory.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
-    TopStories,
+    TopStory,
   },
   data: () => {
     return {
@@ -83,17 +31,26 @@ export default {
     };
   },
   mounted() {
-    this.$http
-      .get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
-
-      .then(function(item) {
-        this.items = item.body;
-      })[0]
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+      .then((res) => res.json())
+      .then((item) => {
+        this.items = item;
+      })
       .catch(function(error) {
-        console.log("Error", error);
+        console.log("Error:", error);
       });
 
-    console.log(this.item);
+    // this.$http
+    //   .get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+
+    //   .then(function(item) {
+    //     this.items = item.body;
+    //   })
+    //   .catch(function(error) {
+    //     console.log("Error", error);
+    //   });
+
+    // console.log(this.item);
   },
 };
 </script>
